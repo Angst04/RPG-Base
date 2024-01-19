67bd7eca1790607e9372ff14def4b8e22cbe936a
Justifall <84130430+Angst04@users.noreply.github.com>
2024-01-19 18:47:51 +0300

    connect to selectel

--- core/dbs_config.py
+++ core/dbs_config.py
@@ -1,4 +1,9 @@
-host = '127.0.0.1'
+host = '5.35.5.39'
 user = 'postgres'
 password = 'angst0437'
 db_name = 'postgres'
+
+# host = '127.0.0.1'
+# user = 'stas'
+# password = 'angst0437'
+# db_name = 'postgres'
