23ec230665a062f7200d39497c81160d4b496607
Justifall <84130430+Angst04@users.noreply.github.com>
2024-01-23 16:15:30 +0300

    connect to new db

--- core/dbs_config.py
+++ core/dbs_config.py
@@ -1,7 +1,7 @@
-host = '5.35.5.39'
-user = 'postgres'
-password = 'angst0437'
-db_name = 'postgres'
+host = "77.105.174.144"
+user = "postgres"
+password = "angst0437"
+db_name = "postgres"
 
 # host = '127.0.0.1'
 # user = 'stas'
